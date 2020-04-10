aac2578a7fd2977692d4b5fc9cfa5446252c2360
hazem-taha <hazem@mannai.tech>
2020-04-10 14:41:27 +0300

    first commit

---
+++ src/config.js
@@ -0,0 +1,57 @@
+export const messages = {
+  login:
+    "Login as any user on this computer and another user on another computer.",
+  create_session: "Creating a session...",
+  connect: "Connecting...",
+  connect_error:
+    "Something went wrong with the connection. Check internet connection or user info and try again.",
+  login_as: "Logged in as ",
+  title_login: "Choose a user to login with:",
+  title_callee: "Choose users to call:",
+  calling: "Calling...",
+  webrtc_not_avaible: "WebRTC is not available in your browser",
+  no_internet: "Please check your Internet connection and try again",
+  select_more_users: "Select at less one user to start Videocall",
+  share_call_link: "Share the above url with the users you want to have a call with",
+  prompt_user_name: "Input user name",
+  confirm_cancel: "Do you shure to leave the call ?"
+};
+
+export const credentials = {
+  appId: 2155,
+  authKey: "HhpRrHk79-HmPLE",
+  authSecret: "YV-qmp2JPACSdfa"
+};
+
+export const appConfig = {
+  debug: { mode: 1 }
+};
+
+export const users = [
+  {
+    id: 1249124,
+    name: "Omar",
+    login: "omar",
+    password: "asdasd123123",
+    color: "#43a70f"
+  },
+  {
+    id: 1249126,
+    name: "Malick",
+    login: "malick",
+    password: "asdasd123123",
+    color: "#077988"
+  },
+  {
+    id: 1250795,
+    name: "Khalifa",
+    login: "kmannai",
+    password: "asdasd123123",
+    color: "#884322"
+  }
+];
+
+export const GUEST_ROOM_ONLY_MODE = false
+
+export const NO_ASNWER_TIMER = 30000 // 30 sec
+
